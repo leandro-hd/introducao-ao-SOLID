26442aaa91b4e80fd51e1e0567995056d756fd7a
leandro-hd <leandrohg2003@gmail.com>
2021-10-25 21:10:39 -0300

    chore: Finalizando aplicação de listagem e cadastro de usuários

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -6,10 +6,18 @@ interface IRequest {
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findById(user_id);
+
+    if (!userAlreadyExists) {
+      throw new Error("User not exists!");
+    }
+
+    const user = this.usersRepository.turnAdmin(userAlreadyExists);
+
+    return user;
   }
 }
 
